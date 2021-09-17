7884e52db129b1a1a8cd6517a59a158a03f94eb6
p1607884 <kevin.domingues@etu.univ-lyon1.fr>
2021-09-17 16:42:10 +0200

    description des gérents de la salle

--- src/Membres.js
+++ src/Membres.js
@@ -16,6 +16,11 @@ const useStyles = makeStyles((theme) => ({
     margin: '10px 5vh 10px 5vh',
     alignItems: 'center',
   },
+  medium: {
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+    alignSelf: 'center',
+  },
   large: {
     width: theme.spacing(7),
     height: theme.spacing(7),
@@ -31,6 +36,8 @@ export default function Membres() {
       <Title>Membres</Title>
       <p>Sont représenté ici les membres les plus actifs/historiques/importants:</p>
 
+      <Box borderTop={1} borderColor='#858585'/>
+
       <Row color={'#500707'} style={{alignSelf: 'center'}}>
         <h2>Gérants de la salle</h2>
       </Row>
@@ -40,11 +47,11 @@ export default function Membres() {
           <h3>Major72</h3>
         </Column>
         <Column className={classes.avatar}>
-          <Avatar alt="Skrafft profile picture" src="https://cdn.discordapp.com/avatars/132840204032540673/3ec0518f79d87e6ce7e72ebcb4df3c02.png" className={classes.large} />
+          <Avatar alt="Skrafft profile picture" src="https://cdn.discordapp.com/avatars/132840204032540673/5afa10f9975608d167450e673d12a37c.png?" className={classes.large} />
           <h3>Skrafft</h3>
         </Column>
         <Column className={classes.avatar}>
-          <Avatar alt="Sora16 profile picture" src="https://cdn.discordapp.com/avatars/282925902965374976/2e87b095831259a24002d8bf536eff30.png?size=128" className={classes.large} />
+          <Avatar alt="Sora16 profile picture" src="https://cdn.discordapp.com/avatars/282925902965374976/2e87b095831259a24002d8bf536eff30.png?" className={classes.large} />
           <h3>Sora16</h3>
         </Column>
       </Row>
@@ -97,9 +104,48 @@ export default function Membres() {
         </Column>
       </Row>
 
-      <Box borderTop={1} borderColor='#858585'/>
+      <Box borderTop={1} borderColor='#858585' style={{margin:'10px 0 20px 0'}}/>
 
-      <p> Helllo</p>
+      <Row color={'#500707'}>
+        <Column style={{margin:'0 10px 5px 10px'}}>
+          <Avatar alt="Major72 profile picture" src="https://cdn.discordapp.com/avatars/254736093239443467/3aa9280c94f1df2b43d9b00e20c365ed.png" className={classes.medium}/>
+          <h4 style={{margin:'10px 0 0 0'}}>Major72</h4>
+        </Column>
+        <Box borderLeft={1} borderColor='#858585'/>
+        <Column color={'#000000'} style={{margin:'0 0 0 10px'}}>
+          <p> Fondateur du server et membre du groupe "<font style={{ fontWeight: 'bold', color:'#d0a7db'}}>Les Suceuses de l'Ouest</font>".<br/>
+              Expert sur Minecraft vanilla mais aussi sur le mod <a href="https://www.curseforge.com/minecraft/mc-mods/botania" target="_blank" style={{ fontWeight: 'bold'}}>Botania</a> et Pirate <font style={{ fontWeight: 'bold', color:'#38b988'}} >Athena</font> sur <font style={{ fontWeight: 'bold', color:'#38b988'}} >Sea of thieves</font>.
+          </p>
+        </Column>
+      </Row>
+
+      <Row color={'#500707'}>
+        <Column style={{margin:'0 10px 5px 10px'}}>
+          <Avatar alt="Skrafft profile picture" src="https://cdn.discordapp.com/avatars/132840204032540673/5afa10f9975608d167450e673d12a37c.png?" className={classes.medium} />
+          <h4 style={{margin:'10px 8px 0 0'}}>Skrafft</h4>
+        </Column>
+        <Box borderLeft={1} borderColor='#858585'/>
+        <Column color={'#000000'} style={{margin:'0 0 0 10px'}}>
+          <p> Membre initial du server et membre du groupe "<font style={{ fontWeight: 'bold', color:'#d0a7db'}}>Les Putes à Frites</font>" car il est notre réprésentant belge.<br/>
+              Joueur <font style={{ fontWeight: 'bold', color:'red'}}>Valorant </font> classé Silver et Pirate <font style={{ fontWeight: 'bold', color:'#38b988'}} >Athena</font> sur <font style={{ fontWeight: 'bold', color:'#38b988'}} >Sea of thieves</font>.<br/>
+              Spécialisé sur les modes de magie sur Minecraft mode.
+          </p>
+        </Column>
+      </Row>
+
+      <Row color={'#500707'}>
+        <Column style={{margin:'0 10px 5px 10px'}}>
+          <Avatar alt="Sora16 profile picture" src="https://cdn.discordapp.com/avatars/282925902965374976/2e87b095831259a24002d8bf536eff30.png?" className={classes.medium} />
+          <h4 style={{margin:'10px 6px 0 0'}}>Sora16</h4>
+        </Column>
+        <Box borderLeft={1} borderColor='#858585'/>
+        <Column color={'#000000'} style={{margin:'0 0 0 10px'}}>
+          <p> Membre initial du server et membre du groupe "<font style={{ fontWeight: 'bold', color:'#d0a7db'}}>Les Suceuses de l'Ouest</font>".<br/>
+              Joueur pro sur <font style={{ fontWeight: 'bold', color:'brown'}}>COD</font> et <font style={{ fontWeight: 'bold', color:'orange'}}>Rocket league</font>, participant à différents tournois.
+          </p>
+        </Column>
+      </Row>
+
 
     </React.Fragment>
   );
